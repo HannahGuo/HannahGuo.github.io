f70a92c44ae853b93099a276e145e13c9f28b225
HannahGuo <hguo2102@gmail.com>
2022-05-04 22:17:55 -0400

    Updates

---
+++ component---src-pages-404-js-6ad6306df584cad9c007.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_hannah_guo=self.webpackChunkgatsby_hannah_guo||[]).push([[883],{429:function(e,t,n){n.r(t);var u=n(7294),a=n(5670),l=n(9357);t.default=function(){return u.createElement(a.Z,null,u.createElement(l.Z,{title:"404: Not found"}),u.createElement("h1",null,"404: Not Found"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-6ad6306df584cad9c007.js.map
